c01576973c86b974de2241138069ebfd60cad477
ivan <ivan701-1992@mail.ru>
2021-03-30 00:23:18 +0700

    LogsError

--- app/src/main/java/com/gorvodokanalVer1/MyFirebaseMassagingService.java
+++ app/src/main/java/com/gorvodokanalVer1/MyFirebaseMassagingService.java
@@ -1,21 +1,16 @@
 package com.gorvodokanalVer1;
 
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
-import com.gorvodokanalVer1.meters.activity.MainActivity;
 
-import static android.content.ContentValues.TAG;
-import static android.provider.Settings.System.getString;
 
 public class MyFirebaseMassagingService extends FirebaseMessagingService {
 
+ public MyFirebaseMassagingService(){
+         super();
+ }
     @Override
     public void onNewToken(@NonNull String s) {
         super.onNewToken(s);
